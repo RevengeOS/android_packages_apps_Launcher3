43a38360bee5371503f02b9db9c40c6b74d643c8
Chris <chris@cypheros.co>
2018-11-11 02:03:31 -0500

    Launcher3: Introduce Quickspace
    Implements a date/time and weather view on the first page of the
    homescreen similar to Google's Smartspace.
    
    This completely replaces the legacy QSB with our view and uses an open
    source WeatherProvider client to retrieve weather information
    
    Change-Id: I14f2ba42c11c225a961fbcb549510f1a840203c0
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    
    Bootleggers edit: Let's make it toggleable, by moving our showQSB into showQuickspace, using At a Glance strings.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -99,6 +99,7 @@ import com.android.launcher3.model.ModelWriter;
 import com.android.launcher3.notification.NotificationListener;
 import com.android.launcher3.popup.PopupContainerWithArrow;
 import com.android.launcher3.popup.PopupDataProvider;
+import com.android.launcher3.quickspace.QuickSpaceView;
 import com.android.launcher3.shortcuts.DeepShortcutManager;
 import com.android.launcher3.states.InternalStateHandler;
 import com.android.launcher3.states.RotationHelper;
@@ -275,6 +276,9 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
         return mLauncherTab.getClient();
     }
 
+    // QuickSpace
+    private QuickSpaceView mQuickSpace;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         if (DEBUG_STRICT_MODE) {
@@ -355,6 +359,8 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
 
         // For handling default keys
         setDefaultKeyMode(DEFAULT_KEYS_SEARCH_LOCAL);
+
+        mQuickSpace = findViewById(R.id.reserved_container_workspace);
 
         mFeedIntegrationEnabled = isFeedIntegrationEnabled();
         mLauncherTab = new LauncherTab(this, mFeedIntegrationEnabled);
@@ -828,6 +834,10 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
         mModel.refreshShortcutsIfRequired();
 
         DiscoveryBounce.showForHomeIfNeeded(this);
+
+        if (mQuickSpace != null) {
+            mQuickSpace.onResume();
+        }
 
         if (mFeedIntegrationEnabled) {
             mLauncherTab.getClient().onResume();
@@ -1898,12 +1908,12 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
     @Override
     public void bindScreens(ArrayList<Long> orderedScreenIds) {
         // Make sure the first screen is always at the start.
-        if (Utilities.showQSB(this) &&
+        if (Utilities.showQuickspace(this) &&
                 orderedScreenIds.indexOf(Workspace.FIRST_SCREEN_ID) != 0) {
             orderedScreenIds.remove(Workspace.FIRST_SCREEN_ID);
             orderedScreenIds.add(0, Workspace.FIRST_SCREEN_ID);
             LauncherModel.updateWorkspaceScreenOrder(this, orderedScreenIds);
-        } else if (!Utilities.showQSB(this) && orderedScreenIds.isEmpty()) {
+        } else if (!Utilities.showQuickspace(this) && orderedScreenIds.isEmpty()) {
             // If there are no screens, we need to have an empty screen
             mWorkspace.addExtraEmptyScreen();
         }
@@ -1919,7 +1929,7 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
         int count = orderedScreenIds.size();
         for (int i = 0; i < count; i++) {
             long screenId = orderedScreenIds.get(i);
-            if (!Utilities.showQSB(this) || screenId != Workspace.FIRST_SCREEN_ID) {
+            if (!Utilities.showQuickspace(this) || screenId != Workspace.FIRST_SCREEN_ID) {
                 // No need to bind the first screen, as its always bound.
                 mWorkspace.insertNewWorkspaceScreenBeforeEmptyScreen(screenId);
             }
